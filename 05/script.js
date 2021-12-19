9f20913ae0a0fa7754c5f04292e819d5a36a85ce
Ana Maria <ana.nikoly@gmail.com>
2021-12-18 22:40:47 -0300

    Operadores

---
+++ 05/script.js
@@ -0,0 +1,17 @@
+var valorUm = 10;
+var valorDois = 3;
+
+var totalAdicao = valorUm + valorDois;
+document.write("A soma de " + valorUm + " + " + valorDois + " é  " + totalAdicao + "<br>" );
+
+var totalSubtracao = valorUm - valorDois;
+document.write("A Subtracao de " + valorUm + " - " + valorDois + " é  " + totalSubtracao + "<br>"  );
+
+var totalMultiplicacao = valorUm * valorDois;
+document.write("A Multiplicacao de " + valorUm + " * " + valorDois + " é  " + totalMultiplicacao + "<br>"  );
+
+var totalDivisao = valorUm / valorDois;
+document.write("A Divisao de " + valorUm + " / " + valorDois + " é  " + totalDivisao.toFixed(2) + "<br>"  );
+
+var totalResto = valorUm % valorDois;
+document.write("O Resto de " + valorUm + " % " + valorDois + " é  " + totalResto + "<br>"  );
