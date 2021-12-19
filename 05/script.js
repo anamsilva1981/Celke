126f9d70370e78725e93b305a6962c139b7f7989
Ana Maria <ana.nikoly@gmail.com>
2021-12-18 23:17:12 -0300

    atribuição

--- 05/script.js
+++ 05/script.js
@@ -1,17 +1,22 @@
 var valorUm = 10;
 var valorDois = 3;
 
+/*Operador de Atribuição*/
 var totalAdicao = valorUm + valorDois;
 document.write("A soma de " + valorUm + " + " + valorDois + " é  " + totalAdicao + "<br>" );
 
+/*Operador de Subtração*/
 var totalSubtracao = valorUm - valorDois;
 document.write("A Subtracao de " + valorUm + " - " + valorDois + " é  " + totalSubtracao + "<br>"  );
 
+/*Operador de Multiplicação*/
 var totalMultiplicacao = valorUm * valorDois;
 document.write("A Multiplicacao de " + valorUm + " * " + valorDois + " é  " + totalMultiplicacao + "<br>"  );
 
+/*Operador de Divisão*/
 var totalDivisao = valorUm / valorDois;
 document.write("A Divisao de " + valorUm + " / " + valorDois + " é  " + totalDivisao.toFixed(2) + "<br>"  );
 
+/*Operador de Resto*/
 var totalResto = valorUm % valorDois;
 document.write("O Resto de " + valorUm + " % " + valorDois + " é  " + totalResto + "<br>"  );
---
+++ 06/script.js
@@ -0,0 +1,20 @@
+var valorUm = 8;
+var valorDois = 3;
+
+valorUm += valorDois;
+document.write("Operador de atribuição com soma: " + valorUm + "<br>");
+
+valorUm -= valorDois;
+document.write("Operador de atribuição com subtração: " + valorUm + "<br>");
+
+valorUm *= valorDois;
+document.write("Operador de atribuição com multiplicação: " + valorUm + "<br>");
+
+valorUm /= valorDois;
+document.write("Operador de atribuição com divisão: " + valorUm + "<br>");
+
+valorUm %= valorDois;
+document.write("Operador de atribuição com módulo: " + valorUm + "<br>");
+
+valorUm = valorDois;
+document.write("Operador de atribuição com aribuição simples: " + valorUm + "<br>");
