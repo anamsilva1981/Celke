a3a02e4d86738eb9c0dbb5f01a7ae3af65b96ab7
Ana Maria <ana.nikoly@gmail.com>
2021-12-19 21:39:53 -0300

    while

---
+++ 12 - While/script.js
@@ -0,0 +1,8 @@
+var inicio = 1;
+
+while(inicio <= 10){
+    document.write("Numero do cliente: " + inicio + "<br>");
+    inicio++;
+}
+
+/*Enquanto a variável inicio for menor que 10 ela será incremetada*/
